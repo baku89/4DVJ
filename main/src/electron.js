112928f591ef1d78e9f547e1e904bce06e73a79b
baku89 <mail@baku89.com>
2015-12-02 08:16:33 +0900

    Update settings for MBP

--- main/src/electron.js
+++ main/src/electron.js
@@ -11,8 +11,8 @@ app.on('ready', () => {
 	mainWindow = new BrowserWindow({width: 800, height: 600})
 	mainWindow.openDevTools()
 	// mainWindow.loadUrl('file://' + __dirname + '/index.html')
-	mainWindow.loadUrl('file:///Volumes/MugiRAID1/Works/2015/29_chanel/0b/main/build/index.html')
-	// mainWindow.loadUrl('file:///Users/mugi/Works/2015/25_chanel/0b/main/build/index.html')
+	// mainWindow.loadUrl('file:///Volumes/MugiRAID1/Works/2015/29_chanel/0b/main/build/index.html')
+	mainWindow.loadUrl('file:///Users/mugi/Works/2015/25_chanel/0b/main/build/index.html')
 	mainWindow.on('closed', () => {
 		mainWindow = null
 	})
